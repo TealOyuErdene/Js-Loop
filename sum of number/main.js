59bc92cd5ec3929c2265796354ee9a49bc80d2f0
TealOyuErdene <118173533+TealOyuErdene@users.noreply.github.com>
2022-12-04 23:18:31 +0800

    Add files via upload

---
+++ sum of number/main.js
@@ -0,0 +1,14 @@
+// 1. Prompt - оор 1 тоо авна.
+// 2. 1 ээс promp оор авсан тоо хүртэлх нийлбэрийг ол.
+// example: input = 10 
+// 1+2+3+4+5+6+7+8+9+10 = 55;
+
+let n = prompt('Enter your number')
+let i = 0;
+let b = 0;
+
+while(n > i){
+    i++
+    b = b + i
+    console.log(b)
+}
