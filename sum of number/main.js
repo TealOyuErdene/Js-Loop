d7fc76151e31cf9e6746862db0be411bb3a2a066
TealOyuErdene <118173533+TealOyuErdene@users.noreply.github.com>
2022-12-04 23:13:40 +0800

    Add files via upload

--- sum of number/main.js
+++ sum of number/main.js
@@ -3,11 +3,12 @@
 // example: input = 10 
 // 1+2+3+4+5+6+7+8+9+10 = 55;
 
-let number = prompt('Enter your number')
+let n = prompt('Enter your number')
 let i = 0;
+let b = 0;
 
-while(number > 1){
+while(n > i){
     i++
-    console.log(number + i);
+    b = b + i
+    console.log(b)
 }
-
