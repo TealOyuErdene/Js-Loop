a9f659975c42b13e23a2e5ec52b4a4625274b862
TealOyuErdene <118173533+TealOyuErdene@users.noreply.github.com>
2022-12-06 02:14:21 +0800

    .

---
+++ While loop/main.js
@@ -0,0 +1,156 @@
+// //1. 10 хүртэлх натурал тоог хэвлэх программ бич
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+
+// //2. 10 хүртэлх натурал тооны сондгой тоонуудыг хэвлэх программ бич
+// for(let i = 0; i <= 10; i++ ){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+
+
+
+// // 3. 10 хүртэлх натурал тооны тэгш тоонуудыг хэвлэх программ бич
+// for(let i = 0; i <= 10; i++){
+//     if(i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+
+
+
+// // 4. 100 хүртэлх натурал тооны нийлбэрийг олох программ бич
+// let sum = 0
+// for(let i = 0; i <= 100; i++){
+//     if(i <= 100){
+//         sum = sum + i
+//     }
+// }
+// console.log(sum)
+
+
+
+// // 5. N хүртэлх натурал тооны хэвлэх болон тэдгээр тооны нийлбэрийг олох программ бич
+// let n = prompt('Enter your number')
+// let sumNumber = 0
+// for(let i = 0; i <= n; i++){
+//     if(i <= n){
+//         sumNumber = sumNumber + i
+//         console.log(i)
+//     }
+// }
+// console.log(sumNumber)
+
+
+
+// // 6. Өгөгдсөн тоог анхны мөн эсэхийг олох программ бич
+// let m = prompt('Enter your number')
+// let count = 0
+// for(let i = 0; i <= m; i++  ){
+//     if(m % i == 0){
+//         count++
+//     }
+// }
+// if(count == 2){
+//     console.log('Anhnii too mun')
+// }else{
+//     console.log('Anhnii too bish')
+// }
+
+
+
+// 7. Өгөгдсөн интервал дахь анхны тоог хэвлэх программ бич   baga zereg asuudaltai
+// let x = prompt('Enter your min number')
+// let y = prompt('Enter your max number')
+// let result = 0
+// for(let i = 10; i <= y; i++ ){
+//     if(y % i == 0){
+//         result++
+//     }
+// }
+// if(result == 2){
+//     console.log(i)
+// }else{
+//     console.log('Anhnii too oldsongui')
+// }
+
+
+
+// // 8. N тооны факториал олох программ бич
+// let Nnumber = prompt('Enter your n number')
+// let mul = 1
+// for(let i = 1; i <= Nnumber; i++){
+//     mul= mul * i
+// }
+// console.log(mul)
+
+
+
+// 9. Өгөгдсөн тооны цифрүүдийн нийлбэрийг олох программ бич
+
+
+
+// // 10. N хүртэлх тооны сондгой тоо хэвлэх болон түүний нийлбэрийг олох программ бич
+// let oddNumber = prompt('Enter your number')
+// let oddSum = 0
+// for(let i = 1; i <= oddNumber; i++){
+//     if(i % 2 != 0){
+//         oddSum = oddSum + i
+//         console.log(i)
+//     }
+// }
+// console.log(oddSum)
+
+
+
+// // 11. N хүртэлх тооны тэгш тоо хэвлэх болон түүний нийлбэрийг олох программ бич
+// let evenNumber = prompt('Enter your number')
+// let evenSum = 0
+// for(let i = 1; i <= evenNumber; i++){
+//     if(i % 2 == 0){
+//         evenSum = evenSum + i
+//         console.log(i)
+//     }
+// }
+// console.log(evenSum)
+
+
+
+// 12. Өгөгдсөн  эерэг тооны урвууг хэвлэх программ бич // 123 <=> 321
+// 13. Өгөдсөн тооны цифрүүдийг үгээр хэвлэх программ бич
+
+
+
+
+
+
+
+
+
+
+
+//interval
+// for(let i = 10; i <= 60; i++){
+//     if(i % 3 == 0 && i % 4 == 0){
+//         console.log(i)
+//     }
+// }
+
+// let sum = 0
+// let mul = 1
+// for(let i = 1; i <= 15; i++){
+//     if(i % 2 == 0 ){
+//         sum = sum + i
+//     }
+
+//     if(i % 2 != 0){
+//         mul = mul * i
+//     }
+// }
+// console.log(sum)
+// console.log(mul)
+
